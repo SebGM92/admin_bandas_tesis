e076db251bcebb369d948c3382c976879d9f7744
Sebastian Guzman Muñoz <seguzmu@gmail.com>
2026-08-25 18:46:34 -0400

    Corrige error de CORS añadiendo permisos para el frontend en settings.py

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -134,7 +134,9 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 
 
 # --- CONFIGURACIÓN DE CORS ---
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 
