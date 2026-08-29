834e4c15c776fddaf7eb3b8f2be8094efe49231f
Sebastian Guzman Muñoz <seguzmu@gmail.com>
2026-08-28 23:28:44 -0400

    fix: Remueve header Authorization en login de Google para evitar error 401

--- frontend/frontend/app/login/page.tsx
+++ frontend/frontend/app/login/page.tsx
@@ -22,8 +22,7 @@ function ContenedorLogin() {
                 const res = await fetch("http://127.0.0.1:8000/api/v1/usuarios/google/", {
                     method: "POST",
                     headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+                        "Content-Type": "application/json"
                     },
                     body: JSON.stringify({
                         username: "SebaGuzman", // Recuerda probar sin espacios
